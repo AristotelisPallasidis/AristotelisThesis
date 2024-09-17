a01899f0b0867b26e82977c8e90c6c6ad03c9f9c
Aristotelis Pallasidis <aristotelis.pallasid@gmail.com>
2024-09-17 12:49:55 +0300

    Added the EntityFamework Base
    - Requirements
    - The DB ready
    - Added the Class library on the solution

---
+++ AristotelisThesis.EntityFramework/AristotelisThesisDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AristotelisThesis.EntityFramework
+{
+    public class AristotelisThesisDbContext : DbContext
+    {
+        //public DbSet<> dbSet { get; set; }
+    }
+}
