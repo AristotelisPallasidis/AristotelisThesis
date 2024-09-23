1f1fe690dbda33b6cfb8bee507dbf0db2ee314ce
Aristotelis Pallasidis <aristotelis.pallasid@gmail.com>
2024-09-23 23:58:37 +0300

    Feature: Simple Clock Counter implementation
    - Added a simple clock counter to the code this is the first thing chat did, in the near future do more research to finish this

--- AristotelisThesis.WPF/Controls/ClockCard.xaml.cs
+++ AristotelisThesis.WPF/Controls/ClockCard.xaml.cs
@@ -1,28 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.ComponentModel;
 
 namespace AristotelisThesis.WPF.Controls
 {
-    /// <summary>
-    /// Interaction logic for ClockCard.xaml
-    /// </summary>
-    public partial class ClockCard : UserControl
+    public partial class ClockCard : UserControl, INotifyPropertyChanged
     {
+        private DispatcherTimer _timer;
+        private TimeSpan _sessionDuration;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _sessionTime;
+        public string SessionTime
+        {
+            get { return _sessionTime; }
+            set
+            {
+                _sessionTime = value;
+                OnPropertyChanged(nameof(SessionTime));
+            }
+        }
+
         public ClockCard()
         {
             InitializeComponent();
+            DataContext = this; // Set DataContext for data binding
+
+            _sessionDuration = TimeSpan.Zero;
+            SessionTime = _sessionDuration.ToString(@"hh\:mm\:ss");
+
+            StartSessionTimer();
+        }
+
+        private void StartSessionTimer()
+        {
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1) // Set the timer to tick every second
+            };
+
+            _timer.Tick += (s, e) =>
+            {
+                _sessionDuration = _sessionDuration.Add(TimeSpan.FromSeconds(1));
+                SessionTime = _sessionDuration.ToString(@"hh\:mm\:ss"); // Update session time
+            };
+
+            _timer.Start(); // Start the timer
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
